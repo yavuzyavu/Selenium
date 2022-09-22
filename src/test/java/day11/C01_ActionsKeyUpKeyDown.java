fccff9da7da6bd7f4a45e0838e03740786a4e3a9
Abdullah Kadioglu <abdkadioglu21@gmail.com>
2022-09-22 23:53:12 +0300

    day12

---
+++ src/test/java/day11/C01_ActionsKeyUpKeyDown.java
@@ -0,0 +1,28 @@
+package day11;
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import utilities.TestBaseBeforeAfter;
+public class C01_ActionsKeyUpKeyDown extends TestBaseBeforeAfter {
+    @Test
+    public void test1() {
+        //https://www.amazon.com sayfasina gidelim
+        driver.get("https://www.amazon.com");
+        //Arama kutusuna actions method’larine kullanarak  samsung A71 yazdirin ve Enter’a basarak arama  yaptirin
+        //Actions actions = new Actions(driver);
+        WebElement aramaKutusu=driver.findElement(By.id("twotabsearchtextbox"));
+        actions.click(aramaKutusu).
+                sendKeys("samsung").
+                sendKeys(" ").
+                keyDown(Keys.SHIFT).// shift tusu otomasyon ile bu sekilde kullanılır. Shift tuşuna basmak için keyDown,
+                sendKeys("a").keyUp(Keys.SHIFT).// shift ten çıkmak için keyUp kullanılır
+                sendKeys("7").sendKeys("1").sendKeys(Keys.ENTER).
+                perform();
+        //aramanin gerceklestigini test edin
+        WebElement aramaSonucu=driver.findElement(By.xpath("(//div[@class='sg-col-inner'])[1]"));
+        Assert.assertTrue(aramaSonucu.isDisplayed());
+    }
+}
